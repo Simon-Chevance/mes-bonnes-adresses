18099a7f90d78f1c07547bf240d940bee4c97704
Thomas <8051201-tbergon@users.noreply.gitlab.com>
2024-01-20 14:34:57 +0100

    Liste/ajout/suppression d'adresses

--- app/screens/AddressList.jsx
+++ app/screens/AddressList.jsx
@@ -1,53 +1,100 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList, TouchableOpacity, Modal, TextInput } from 'react-native';
-import { db } from '../../firebase';
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import {View, Text, Button, FlatList, TouchableOpacity, Modal, TextInput, Switch, Alert} from 'react-native';
+import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { GeoPoint } from "firebase/firestore"
 
-const AddressListItem = ({ address, onPress }) => (
+const db = getFirestore();
+
+const AddressListItem = ({ address, onPress, onDelete }) => (
     <TouchableOpacity onPress={onPress}>
         <View style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: '#ccc' }}>
             <Text>{address.name}</Text>
             <Text>{address.description}</Text>
         </View>
+        <Button title="Supprimer" onPress={() => onDelete(address.id)} />
     </TouchableOpacity>
 );
 
 const AddressCreationModal = ({ isVisible, onClose, onCreateAddress }) => {
     const [newAddressName, setNewAddressName] = useState('');
     const [newAddressDescription, setNewAddressDescription] = useState('');
+    const [newAddressIsPublic, setNewAddressIsPublic] = useState(false);
+    const [newAddressLatitude, setNewAddressLatitude] = useState('');
+    const [newAddressLongitude, setNewAddressLongitude] = useState('');
+    const [newAddressPhotoURL, setNewAddressPhotoURL] = useState('');
 
     const handleCreateAddress = async () => {
+        const latitude = parseFloat(newAddressLatitude);
+        const longitude = parseFloat(newAddressLongitude);
+
+        if (isNaN(latitude) || isNaN(longitude)) {
+            console.error('Invalid latitude or longitude values');
+            return;
+        }
+
         try {
             const docRef = await addDoc(collection(db, 'addresses'), {
                 name: newAddressName || 'New Address',
                 description: newAddressDescription || 'Address Description',
+                isPublic: newAddressIsPublic,
+                location: new GeoPoint(latitude, longitude),
+                photoURL: newAddressPhotoURL || 'photoURL'
             });
+            console.log('Created document: ', docRef.id);
 
-            console.log('Document written with ID: ', docRef.id);
             setNewAddressName('');
             setNewAddressDescription('');
+            setNewAddressIsPublic(false);
+            setNewAddressLatitude('');
+            setNewAddressLongitude('');
+            setNewAddressPhotoURL('');
             onCreateAddress();
+            Alert.alert('Succès', 'Adresse ajoutée');
+            onClose();
         } catch (e) {
             console.error('Error adding document: ', e);
+            Alert.alert('Erreur', "Erreur lors de l'ajout de l'adresse");
         }
     };
 
     return (
         <Modal visible={isVisible} onRequestClose={onClose}>
             <View style={{ padding: 20 }}>
-                <Text>Create New Address</Text>
+                <Text>Ajouter une adresse</Text>
                 <TextInput
-                    placeholder="Address Name"
+                    placeholder="Nom"
                     value={newAddressName}
                     onChangeText={(text) => setNewAddressName(text)}
                 />
                 <TextInput
-                    placeholder="Address Description"
+                    placeholder="Description"
                     value={newAddressDescription}
                     onChangeText={(text) => setNewAddressDescription(text)}
                 />
-                <Button title="Create" onPress={handleCreateAddress} />
-                <Button title="Cancel" onPress={onClose} />
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Text>Is Public:</Text>
+                    <Switch
+                        value={newAddressIsPublic}
+                        onValueChange={(value) => setNewAddressIsPublic(value)}
+                    />
+                </View>
+                <TextInput
+                    placeholder="Latitude"
+                    value={newAddressLatitude}
+                    onChangeText={(text) => setNewAddressLatitude(text)}
+                />
+                <TextInput
+                    placeholder="Longitude"
+                    value={newAddressLongitude}
+                    onChangeText={(text) => setNewAddressLongitude(text)}
+                />
+                <TextInput
+                    placeholder="Photo"
+                    value={newAddressPhotoURL}
+                    onChangeText={(text) => setNewAddressPhotoURL(text)}
+                />
+                <Button title="Créer" onPress={handleCreateAddress} />
+                <Button title="Annuler" onPress={onClose} />
             </View>
         </Modal>
     );
@@ -58,7 +105,7 @@ const AddRemoveAddress = ({ addresses, onAddAddress, onRemoveAddress }) => {
 
     return (
         <View>
-            <Button title="Add Address" onPress={() => setModalVisible(true)} />
+            <Button title="Ajouter" onPress={() => setModalVisible(true)} />
             <AddressCreationModal
                 isVisible={isModalVisible}
                 onClose={() => setModalVisible(false)}
@@ -69,7 +116,11 @@ const AddRemoveAddress = ({ addresses, onAddAddress, onRemoveAddress }) => {
                     data={addresses}
                     keyExtractor={(item) => item.id}
                     renderItem={({ item }) => (
-                        <AddressListItem address={item} onPress={() => onRemoveAddress(item.id)} />
+                        <AddressListItem
+                            address={item}
+                            onPress={() => console.log('clic sur une adresse')}
+                            onDelete={onRemoveAddress}
+                        />
                     )}
                 />
             )}
@@ -83,34 +134,20 @@ const AddressList = () => {
     const [error, setError] = useState(null);
 
     const fetchAddresses = async () => {
-        console.log("fetchAddresses start")
-        console.log("db : ", db)
-        let addressCollection;
         try {
-            addressCollection = collection(db, "addresses");
-            console.log("collection : ", addressCollection)
-
-            try {
-                const querySnapshot = await getDocs(addressCollection);
-                querySnapshot.forEach((doc) => {
-                    console.log(`${doc.id} => ${doc.data()}`);
-                });
-                /*
-                                const querySnapshot = await getDocs(collection(db, 'addresses'));
-                                console.log('Query Snapshot:', querySnapshot);
-                                const addresses = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-                                setAddressList(addresses);
-                                setLoading(false);
-                                setError(null);
-
-                 */
-            } catch (err) {
-                console.error('Error fetching addresses: ', err);
-                setLoading(false);
-                setError(err.message);
-            }
+            const querySnapshot = await getDocs(collection(db, "addresses"));
+            console.log('Query Snapshot:', querySnapshot);
+            const addresses = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setAddressList(addresses);
+            setLoading(false);
+            setError(null);
         } catch (err) {
-            console.error("retrieving collection error : ", err)
+            console.error('Error fetching addresses: ', err);
+            setLoading(false);
+            setError(err.message);
         }
     };
 
@@ -118,16 +155,20 @@ const AddressList = () => {
         fetchAddresses();
     }, []);
 
-    const addAddress = async (newAddress) => {
+    const addAddress = async () => {
+        fetchAddresses();
     };
 
     const removeAddress = async (id) => {
         try {
-            await db.collection('addresses').doc(id).delete();
+            await deleteDoc(doc(db, 'addresses', id));
             setError(null);
+            fetchAddresses();
+            Alert.alert('Succès', "Suppression de l'adresse");
         } catch (error) {
             console.error('Error removing address:', error);
             setError(error.message);
+            Alert.alert('Erreur', "Erreur lors de la suppression de l'adresse");
         }
     };
 
